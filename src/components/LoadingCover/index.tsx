ad876163c6d23a94183b0861b00959c8452d8b26
Kaz Watanabe <kwatanabe@heartbeats.jp>
2020-07-14 06:25:00 +0900

    remove styled component

--- src/components/LoadingCover/index.tsx
+++ src/components/LoadingCover/index.tsx
@@ -1,32 +1,15 @@
 import React from 'react'
-import { CircularProgress, Box, Theme, withStyles } from '@material-ui/core'
-import { colors } from '~/theme'
+import { CircularProgress, Box } from '@material-ui/core'
+import { useStyles } from './styles'
 
-const LoadingCover: React.FC = (props) => (
-  <Wrapper {...props}>
-    <StyledCircularProgress />
-  </Wrapper>
-)
+const LoadingCover: React.FC = (props) => {
+  const classes = useStyles()
 
-const Wrapper = withStyles((theme: Theme) => ({
-  root: {
-    backgroundColor: colors.coverBackground,
-    width: '100%',
-    height: '100%',
-    position: 'relative',
-    textAlign: 'center',
-  },
-}))(Box)
-
-const StyledCircularProgress = withStyles((theme: Theme) => ({
-  root: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-    margin: 'auto',
-  },
-}))(CircularProgress)
+  return (
+    <Box className={classes.wrapper} {...props}>
+      <CircularProgress className={classes.progress} />
+    </Box>
+  )
+}
 
 export default LoadingCover
