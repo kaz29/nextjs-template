c69ddb691d851163ece56bfdecc3525eb9774ab6
田村行生 <>
2020-10-07 14:33:05 +0900

    yarn add --dev next-transpile-modules and fix transpile

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 // @ts-ignore
 const withImages = require('next-images')
 
-module.exports = withImages({
+const withTM = require('next-transpile-modules')([
+  'query-string',
+  'split-on-first',
+  'strict-uri-encode',
+])
+
+module.exports = withTM(withImages({
   publicRuntimeConfig: {
     WEATHER_API_ENDPOINT: process.env.WEATHER_API_ENDPOINT,
     WEATHER_API_KEY: process.env.WEATHER_API_KEY,
   },
-})
+}))
